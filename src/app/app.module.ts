958a7cc9d94d59422a7c4e27d4b12e3261505e5b
aurelienuguen <aurelien.uguen@gmail.com>
2023-05-25 10:38:48 +0200

    routing challenge on angular complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
 import { ValidationButtonComponent } from './validation-button/validation-button.component';
 import { BlockComponentComponent } from './block-component/block-component.component';
+import { MenuComponent } from './menu/menu.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+import { ROUTES } from 'src/app/app.routes';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidationButtonComponent,
     BlockComponentComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
